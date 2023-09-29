c56ce0020444c2812aa45e5ce5f239ce1551c19a
Igor M <eyegor.m@gmail.com>
2023-09-29 20:49:17 +0300

    UPD

--- src/functions.rs
+++ src/functions.rs
@@ -15,34 +15,12 @@ use config::{Config, File, FileFormat};
 //}
 
 pub fn config_parse(string_path: String) -> Result<Vec<String>, Box<dyn Error>> {
-//pub fn config_parse(string_path: String) -> Result<Vec<String>, ConfigError> {
-    //let config_content =
-    //    fs::read_to_string(&config_path).expect("Should have been able to read the file");
-
-    //let config_path = PathBuf::from(string_path.as_str());
-    //let config = DefaultConfigurationBuilder::new()
-    //    .add_json_file(&config_path)
-    //    .build();
     let mut credentials: Vec<String> = Vec::new();
 
     let mut builder = Config::builder();
     builder = builder.set_default("default", "1")?;
     builder = builder.add_source(File::new(&string_path, FileFormat::Json));
     builder = builder.set_override("override", "1")?;
-
-    //match builder.build() {
-    //    Ok(raw_conf) => {
-    //        credentials.push(raw_conf.get("ip").unwrap());
-    //        credentials.push(raw_conf.get("user").unwrap());
-    //        credentials.push(raw_conf.get("password").unwrap());
-
-    //        return Ok(credentials);
-    //    }
-    //    Err(e) => {
-    //        return Err(e);
-    //    }
-    //}
-
     let raw_conf = builder.build().unwrap();
     credentials.push(raw_conf.get("ip").unwrap());
     credentials.push(raw_conf.get("user").unwrap());
