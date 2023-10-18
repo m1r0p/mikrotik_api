ef9dc4dcfe8048af69ea0181a621fd591f2dd27e
Igor M <eyegor.m@gmail.com>
2023-10-18 20:00:05 +0300

    UPD

--- src/functions.rs
+++ src/functions.rs
@@ -6,12 +6,12 @@ pub use structures::Host;
 
 use config::{Config, File, FileFormat};
 //use reqwest::header::{HeaderMap, AUTHORIZATION, CONTENT_TYPE};
+use serde_json::Value;
 use std::error::Error;
-use serde_json::{Value};
 
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
+//fn print_type_of<T>(_: &T) {
+//    println!("{}", std::any::type_name::<T>())
+//}
 
 pub fn get_mikrotik_params(string_path: String) -> Result<Vec<String>, Box<dyn Error>> {
     let mut mikrotik_params: Vec<String> = Vec::new();
@@ -100,16 +100,101 @@ pub async fn get_dhcp_leases(mikrotik_params: Vec<String>) -> Result<Vec<Host>, 
     let password: Option<String> = Some(mikrotik_params[2].to_string());
 
     let resp = client
-        .get(format!("{}{}{}", MIKROTIK_PROTO, mikrotik_params[0], MIKROTIK_DHCP_LEASES))
+        .get(format!(
+            "{}{}{}",
+            MIKROTIK_PROTO, mikrotik_params[0], MIKROTIK_DHCP_LEASES
+        ))
         .basic_auth(user_name, password)
         .send()
         .await?
         .text()
         .await?;
     let hosts_json: Value = serde_json::from_str(resp.as_str()).unwrap();
-    //let hosts_vec: &Vec<Value> = hosts_json["Array"].as_array().unwrap();
-    for i in hosts_json.iter() {
-        println!("{:?}", i);
+    let hosts_vec: &Vec<Value> = hosts_json.as_array().unwrap();
+    for i in hosts_vec.iter() {
+        //let mut address_lists: String = String::new();
+        //if i["address-lists"].as_str().is_empty()  {
+        //    address_lists.push_str("None");
+        //} else {
+        //    address_lists.push_str(i["address-lists"].as_str().unwrap());
+        //}
+        //let mut dhcp_option: String = String::new();
+        //if i["dhcp-option"].as_str().is_empty()  {
+        //    dhcp_option.push_str("None");
+        //} else {
+        //    dhcp_option.push_str(i["dhcp-option"].as_str().unwrap());
+        //}
+        
+        //let mut address_lists: String = String::new();
+        //match i["address-lists"].as_str() {
+        //    None => address_lists.push_str("None"),
+        //    Some(x) => address_lists.push_str(x),
+        //}
+        //let mut dhcp_option: String = String::new();
+        //match i["dhcp-option"].as_str() {
+        //    None => dhcp_option.push_str("None"),
+        //    Some(x) => dhcp_option.push_str(x),
+        //}
+        //let mut age: String = String::new();
+        //match i["age"].as_str() {
+        //    None => age.push_str("None"),
+        //    Some(x) => age.push_str(x),
+        //}
+        //let mut active_address: String = String::new();
+        //match i["active-address"].as_str() {
+        //    None => active_address.push_str("None"),
+        //    Some(x) => active_address.push_str(x),
+        //}
+
+
+        //let host: Host = Host {
+        //    id: i[".id"].as_str().unwrap().to_string(),
+        //    active_address: active_address, 
+        //    active_client_id: i["active-client-id"].as_str().unwrap().to_string(),
+        //    active_mac_address: i["active-mac-address"].as_str().unwrap().to_string(),
+        //    active_server: i["active-server"].as_str().unwrap().to_string(),
+        //    address: i["address"].as_str().unwrap().to_string(),
+        //    address_lists: address_lists,
+        //    age: age,
+        //    blocked: i["blocked"].as_str().unwrap().to_string(),
+        //    client_id: i["client-id"].as_str().unwrap().to_string(),
+        //    dhcp_option: dhcp_option,
+        //    disabled: i["disabled"].as_str().unwrap().to_string(),
+        //    dynamic: i["dynamic"].as_str().unwrap().to_string(),
+        //    expires_after: i["expires-after"].as_str().unwrap().to_string(),
+        //    host_name: i["host-name"].as_str().unwrap().to_string(),
+        //    last_seen: i["last-seen"].as_str().unwrap().to_string(),
+        //    mac_address: i["mac-address"].as_str().unwrap().to_string(),
+        //    radius: i["radius"].as_str().unwrap().to_string(),
+        //    server: i["server"].as_str().unwrap().to_string(),
+        //    status: i["status"].as_str().unwrap().to_string(),
+        //};
+
+        let host: Host = Host {
+            id: i[".id"].as_str().map(Into::into),
+            active_address: i["active-address"].as_str().map(Into::into),
+            active_client_id: i["active-client-id"].as_str().map(Into::into),
+            active_mac_address: i["active-mac-address"].as_str().map(Into::into),
+            active_server: i["active-server"].as_str().map(Into::into),
+            address: i["address"].as_str().map(Into::into),
+            address_lists: i["address-lists"].as_str().map(Into::into),
+            age: i["age"].as_str().map(Into::into),
+            blocked: i["blocked"].as_str().map(Into::into),
+            client_id: i["client-id"].as_str().map(Into::into),
+            dhcp_option: i["dhcp-option"].as_str().map(Into::into),
+            disabled: i["disabled"].as_str().map(Into::into),
+            dynamic: i["dynamic"].as_str().map(Into::into),
+            expires_after: i["expires-after"].as_str().map(Into::into),
+            host_name: i["host-name"].as_str().map(Into::into),
+            last_seen: i["last-seen"].as_str().map(Into::into),
+            mac_address: i["mac-address"].as_str().map(Into::into),
+            radius: i["radius"].as_str().map(Into::into),
+            server: i["server"].as_str().map(Into::into),
+            status: i["status"].as_str().map(Into::into),
+        };
+
+        dhcp_leases.push(host);
+        //println!("{:?}\n\n\n\n", i);
     }
 
     //println!("{:?}", &hosts_json);
